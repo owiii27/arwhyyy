77f6f7c8f4f739ea542970bcc72d4ab0a2e32125
owiii27 <aryanoerdjamilihsanashnaoui@gmail.com>
2025-04-16 05:57:21 +0700

    Add files via upload

---
+++ tugas.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Daftar Produk</title>
+</head>
+<body>
+    <center>
+        <table border="1">
+            <tr>
+                <th colspan="3" bgcolor="orange">DAFTAR PRODUK</th>
+            </tr>
+            <tr>
+                <td rowspan="4" bgcolor="yellow"><img src="/webprogramming1/nike.png" width="200px" alt=""></td>
+                <td bgcolor="greenlime"> Nama <br> Produk</td>
+                <td bgcolor="orchid"> Nike LeBron XXII </td>
+            </tr>
+            <tr>
+                <td bgcolor="greenlime"> Harga</td>
+                <td bgcolor="orchid"> Rp 2,499,000</td>
+            </tr>
+            <tr>
+                <td bgcolor="greenlime">Fitur <br> Produk</td>
+                <td bgcolor="orchid">
+                    <ul>
+                    <li>The Nike LeBron 22 continues to try and build on <br>
+                        the performance of the previous two models but takes a slight step back in one regard.</li>
+                    <li>Traction performance remains top-tier. Most reviewers agree the outsole <br>
+                        grips well and feels safe, especially on cleaner courts.</li>
+                    <li>The cushioning is outstanding for impact protection and bounciness. <br>
+                        The midsole also feels responsive, which is a plus.</li>
+                    <li>Materials are where the shoe regresses. The upper is comfortable and supportive, <br>
+                        but the shoe feels heavier than the previous models.</li>
+                    <li>The fit is accommodating, thanks to the spacious forefoot.</li>
+                    <li>For support and lockdown, Nike implements a unique midfoot saddle to help with stability <br>
+                        and torsional support. Your foot also feels secure during lateral movements.</li>
+                    <li>LeBron's latest signature shoe maintains impressive performance, <br>
+                        highlighted mainly by the traction and cushioning. </li>
+                    </>
+                </td>
+            </tr>
+        </table>
+    </center>
+</body>
+</html>
